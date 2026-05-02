cbd2ac8f042c8456b18896fa4ad35e1b1c43b770
Akshay Kumar <taara0361@gmail.com>
2026-05-02 17:18:00 +0530

    cloudinary intergrated to upload images

--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -5,11 +5,13 @@ import { User } from 'src/user/schema/user.schema';
 import { Model, Types } from 'mongoose';
 import { Blog } from './schema/blog.schema';
 import { MAX } from 'class-validator';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 
 @Injectable()
 export class BlogService {
 
-constructor(@InjectModel(Blog.name) private readonly blogModel:Model<Blog>){}
+constructor(@InjectModel(Blog.name) private readonly blogModel:Model<Blog>,
+private readonly clodinary:CloudinaryService){}
    
 
     /**
@@ -49,9 +51,29 @@ constructor(@InjectModel(Blog.name) private readonly blogModel:Model<Blog>){}
      * @param image and blog dto
      * @return craeted blog
      */
-    async createBlog(image,dto:BlogDto)
+    async createBlog(image:Express.Multer.File,dto:BlogDto)
     {
         
+        const imageUrl=await this.clodinary.uploadImage(image,'test');
+        
+        if(!imageUrl)
+        {
+            throw new BadRequestException('Unable to upload the images..');
+        }
+
+        const blogData=await this.blogModel.create({...dto,imageUrl});
+
+        if(!blogData)
+        {
+            throw new BadRequestException("Unable to create the blog...");
+        }
+
+
+        return {
+            message:"Blog created successfully",
+            data:blogData
+        }
+
     }
 
 
